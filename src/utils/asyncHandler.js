0eb2271787abf3662820f4bc7563154ca8781474
Saurav Jha <srvtj629@gmail.com>
2024-02-22 12:48:03 +0530

    implemented utils, api and error handling

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+export {asyncHandler}
+
+// const asyncHandler = ()=>{}
+// const asyncHandler = (function)=>{()=>{}}
+// const asyncHandler = (function)=>async()=>{}
+
+
+// Logic Using TRY_CATCH
+
+// const asyncHandler = (fn)=>async(req,res,next)=>{
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code||500).json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+// }
+
+// Logic Using PROMISES
+
+const asyncHandler = (requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+}
